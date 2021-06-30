6ae00de54b6f712ead778304f6f15adaca9a3bd6
Ulrich Kautz <ulrich.kautz@gmail.com>
2021-06-30 22:45:21 +0200

    Simplified
    - Interface reduced
      - website mode cannot be changed anymore
      - bucket cannot be provided anymore
    - Build into `dist/`
    - Prep document generation

--- test/static-website.test.ts
+++ test/static-website.test.ts
@@ -3,11 +3,7 @@ import * as cdk from '@aws-cdk/core';
 import * as route53 from '@aws-cdk/aws-route53';
 import * as path from 'path';
 import { assertSnapshot } from './utils';
-
 import * as lib from '../lib/index';
-import * as s3 from '@aws-cdk/aws-s3';
-import { time } from 'console';
-import { prependOnceListener } from 'process';
 
 /*
  * Example test
@@ -25,19 +21,6 @@ describe('Stack Website', () => {
   });
 
   describe('Storage', () => {
-    describe('Bucket Name', () => {
-      // WHEN
-      const stack = createTestStack({
-        bucket: 'my-bucket-name',
-      });
-
-      // THEN
-      assertSnapshot(stack);
-      assertBucket(stack, {
-        BucketName: 'my-bucket-name',
-      });
-    });
-
     describe('Bucket Prefix', () => {
       // WHEN
       const stack = createTestStack({
@@ -59,21 +42,6 @@ describe('Stack Website', () => {
             },
           })
         );
-      });
-    });
-
-    describe('Custom Bucket', () => {
-      // WHEN
-      const stack = createTestStack({}, (stack) => ({
-        bucket: new s3.Bucket(stack, 'Bucket', {
-          bucketName: 'the-pre-existing-bucket',
-        }),
-      }));
-
-      // THEN
-      assertSnapshot(stack);
-      assertBucket(stack, {
-        BucketName: 'the-pre-existing-bucket',
       });
     });
   });
@@ -103,87 +71,6 @@ describe('Stack Website', () => {
       assertCloudFront(stack, {
         errorPage: 'custom-error.html',
         errorCacheDuration: 123,
-      });
-    });
-  });
-
-  describe('Website mode', () => {
-    describe('Disabled', () => {
-      // WHEN
-      const stack = createTestStack({
-        websiteMode: false,
-      });
-
-      // THEN
-      assertSnapshot(stack);
-      test('Bucket website configuration is NOT enabled', () => {
-        expectCDK(stack).to(
-          haveResource('AWS::S3::Bucket', {
-            BucketEncryption: {
-              ServerSideEncryptionConfiguration: [
-                {
-                  ServerSideEncryptionByDefault: {
-                    SSEAlgorithm: 'AES256',
-                  },
-                },
-              ],
-            },
-          })
-        );
-      });
-      test('CloudFront uses S3 origin', () => {
-        expectCDK(stack).to(
-          haveResourceLike('AWS::CloudFront::Distribution', {
-            DistributionConfig: {
-              Origins: [
-                {
-                  S3OriginConfig: undefined,
-                },
-              ],
-            },
-          })
-        );
-      });
-    });
-    describe('Enabled', () => {
-      // WHEN
-      const stack = createTestStack({
-        websiteMode: true,
-      });
-
-      // THEN
-      assertSnapshot(stack);
-      test('Bucket website configuration IS enabled', () => {
-        expectCDK(stack).to(
-          haveResource('AWS::S3::Bucket', {
-            WebsiteConfiguration: {
-              ErrorDocument: 'error.html',
-              IndexDocument: 'index.html',
-            },
-            BucketEncryption: {
-              ServerSideEncryptionConfiguration: [
-                {
-                  ServerSideEncryptionByDefault: {
-                    SSEAlgorithm: 'AES256',
-                  },
-                },
-              ],
-            },
-          })
-        );
-      });
-      test('CloudFront uses custom origin', () => {
-        expectCDK(stack).to(
-          haveResourceLike('AWS::CloudFront::Distribution', {
-            DistributionConfig: {
-              Origins: [
-                {
-                  CustomOriginConfig: undefined,
-                },
-              ],
-            },
-          })
-        );
       });
     });
   });
@@ -237,15 +124,24 @@ function createTestStack(
   return stack;
 }
 
-function assertBucket(stack: cdk.Stack, props?: any) {
-  test('Bucket in place', () => {
-    expectCDK(stack).to(countResources('AWS::S3::Bucket', 1));
-  });
-  if (props) {
-    test('Bucket configured', () => {
-      expectCDK(stack).to(haveResource('AWS::S3::Bucket', props));
-    });
-  }
+function assertBucket(stack: cdk.Stack) {
+  expectCDK(stack).to(
+    haveResource('AWS::S3::Bucket', {
+      WebsiteConfiguration: {
+        ErrorDocument: 'error.html',
+        IndexDocument: 'index.html',
+      },
+      BucketEncryption: {
+        ServerSideEncryptionConfiguration: [
+          {
+            ServerSideEncryptionByDefault: {
+              SSEAlgorithm: 'AES256',
+            },
+          },
+        ],
+      },
+    })
+  );
 }
 
 function assertCloudFront(
